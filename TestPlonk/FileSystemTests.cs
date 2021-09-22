91903e4a493472326e3fa5cd92eea2c11fc9597c
notexplosive <thenotexplosive@gmail.com>
2021-09-22 07:29:11 -0700

    Move tests to own file

---
+++ TestPlonk/FileSystemTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Plonk;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestPlonk
+{
+    public class FileSystemTests
+    {
+
+        [Fact]
+        public void can_change_working_directory()
+        {
+            var fileSystem = new FileSystem(PathType.Absolute, @"C:\temp");
+            fileSystem.WorkingDirectory.Should().Be(@"C:\temp");
+        }
+
+        [Fact]
+        public void can_use_relative_working_directory()
+        {
+            var fileSystem = new FileSystem(PathType.Relative, @"temp");
+            fileSystem.WorkingDirectory.Should().Be(Path.Join(Directory.GetCurrentDirectory(), "temp"));
+        }
+    }
+}
