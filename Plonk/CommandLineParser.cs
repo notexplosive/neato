e019750ef387787e35339747f201abdad23eccb1
notexplosive <thenotexplosive@gmail.com>
2021-09-22 20:06:01 -0700

    CommandLineParser fails to parse invalid args

---
+++ Plonk/CommandLineParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neato
+{
+    public class CommandLineParser
+    {
+        public string Consume(string[] fullArgs)
+        {
+            return string.Format("Usage {0}", Path.Join(Directory.GetCurrentDirectory(), System.AppDomain.CurrentDomain.FriendlyName));
+        }
+    }
+}
---
+++ TestPlonk/CommandLineArgTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Neato;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestNeato
+{
+    public class CommandLineArgTests
+    {
+        [Fact]
+        public void invalid_flag_raises_error()
+        {
+            var parser = new CommandLineParser();
+            var result = parser.Consume(new string[] { "--spujb" });
+
+            result.Should().Be(string.Format("Usage {0}", Path.Join(Directory.GetCurrentDirectory(), System.AppDomain.CurrentDomain.FriendlyName)));
+        }
+    }
+}
