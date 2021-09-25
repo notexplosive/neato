84b8cb81dc2c69796dcf16ce9f7bce5e29f304b0
notexplosive <thenotexplosive@gmail.com>
2021-09-25 10:47:08 -0700

    SevenZip can zip directories

--- Plonk/SevenZipProgram.cs
+++ Plonk/SevenZipProgram.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace Neato
     {
         public SevenZipProgram() : base("7z")
         {
+        }
+
+        public ProgramOutput SendToZip(string buildOutputDirectory, string outputDirectory)
+        {
+            return RunWithArgs("a", "-r", Path.Join(outputDirectory, "output.zip"), Path.Join(buildOutputDirectory, "*"));
         }
     }
 }
---
+++ TestPlonk/SevenZipTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Neato;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestNeato
+{
+    public class SevenZipTests : IDisposable
+    {
+        private readonly string directoryToZip;
+        private readonly string outputDirectory;
+
+        public SevenZipTests()
+        {
+            this.directoryToZip = Path.Join(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString());
+            this.outputDirectory = Path.Join(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(directoryToZip);
+
+            File.Create(Path.Join(directoryToZip, "a.txt")).Close();
+            File.Create(Path.Join(directoryToZip, "b.txt")).Close();
+            File.Create(Path.Join(directoryToZip, "c.txt")).Close();
+        }
+
+        [Fact]
+        public void can_zip_directory()
+        {
+            var sevenZip = new SevenZipProgram();
+            sevenZip.SendToZip(directoryToZip, outputDirectory);
+
+            File.Exists(Path.Join(outputDirectory, "output.zip")).Should().BeTrue();
+
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(directoryToZip))
+            {
+                Directory.Delete(directoryToZip, true);
+            }
+
+            if (Directory.Exists(outputDirectory))
+            {
+                Directory.Delete(outputDirectory, true);
+            }
+        }
+    }
+}
